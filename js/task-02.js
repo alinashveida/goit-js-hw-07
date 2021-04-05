6e29e3d6cf66be64c3d7e030263d7e7066f40078
alinashveida <alinashveida@gmail.com>
2021-04-05 23:51:30 +0300

    переробила 2 завдання

--- js/task-02.js
+++ js/task-02.js
@@ -12,23 +12,6 @@ const ingredients = [
 const ingredientsListEl = document.querySelector('#ingredients')
  console.log(ingredientsListEl);
 
-
-
-// const ingridientEl = ingredients.forEach((ingredient) =>{
-//   const itemEl = document.createElement('li');
-//   itemEl.textContent = ingredient;
-
-//   ingredientsListEl.append(itemEl);
-// })
-//////////////////////////////////////////////////////
-// const ingredientArray = [...ingredients].map(ingredient =>{
-//   const itemEl = document.createElement('li');
-//   itemEl.textContent = ingredient;
-//   return ingredientsListEl.append(itemEl)
-// } )
-
-// console.log(ingredientArray);
-///////////////////////////////////////////////////////////
 const ingridientEl = (ingridient => {
   return `<li> ${ingridient} </li>`
  })
@@ -36,11 +19,3 @@ const ingridientEl = (ingridient => {
 const indridientsList = ingredients.map(ingridientEl).join("");
 
 ingredientsListEl.insertAdjacentHTML("beforeend", indridientsList);
-///////////////////////////////////////////////////////////////////////////
-
-// let d = document.createElement('li');
-// const ingridientEl = (ingridient) => {
-//        return d.textContent = ingredient;
-//    }
-  
-// console.log(ingridientEl)
